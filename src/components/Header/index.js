6611e08f00379f7939be2f466121dbb189b069e0
Vicheka Phat <vickphat@gmail.com>
2021-07-03 11:25:00 -0400

    create components directory:create Header, implemented header into App.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import react from 'react';
+
+const header = () => {
+
+        return (
+            <div>
+                <h1>Employee Directory</h1>
+                <p>
+                    This is the employee directory page!
+                </p>
+            </div> 
+            
+        
+        )
+}
+
+export default header;
