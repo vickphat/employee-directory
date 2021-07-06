72818709bdca5436dc8ac666fb8ff926730e949b
Vicheka Phat <vickphat@gmail.com>
2021-07-05 23:39:30 -0400

    retrieved API objects of random employees

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './components/Header';
 import EmployeesData from './components/EmployeeData';
-import EmployeeTable from './components/EmployeeTable';
 
 
 function App() {
--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -1,44 +1,26 @@
 import React from 'react';
-import axios from 'axios';
 
-const getEmployees = () => {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us") 
-};
-console.log(getEmployees)
-
-const employeeTable = () => {
+const EmployeeTable = (props) => {
     return (
         <table className="table">
   <thead>
     <tr>
-      <th scope="col">Profile Image</th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
+      <th scope="col">Profile</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone</th>
       <th scope="col">Location</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-    )
-}
+  {props.state.filteredEmployees.map((employee) => {
+        
+    console.log(employee);
+    
+        })}
+      </tbody>
+    </table>
+  );
+};
 
-export default employeeTable;
+export default EmployeeTable;
