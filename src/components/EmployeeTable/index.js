b16b2ee85bb3012b4bde5b56345cdcff05bad8a9
Vicheka Phat <vickphat@gmail.com>
2021-07-06 00:44:03 -0400

    rendered employee's email, location, phone number and age

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -3,36 +3,51 @@ import React from 'react';
 const EmployeeTable = (props) => {
     return (
         <table className="table table-striped table-sortable text-center">
-  <thead>
-    <tr>
-      <th scope="col">Profile</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Location</th>
-    </tr>
-  </thead>
-  <tbody>
-  {props.state.filteredEmployees.map((employee) => {
+            <thead>
+                <tr>
+                    <th scope="col">Profile</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Location</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.state.filteredEmployees.map((employee) => {
 
-          const { first, last } = employee.name;
-          const name = `${first} ${last}`;
-        
-          console.log(employee);
-    
-    return (
-            <tr key={employee.login.uuid}>
-              <td>
-                <img src={employee.picture.medium} alt={name} />
-              </td>
-              <td className="align-middle">{name}</td>
-              
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+                    const { first, last } = employee.name;
+                    const name = `${first} ${last}`;
+                    const { city, state } = employee.location
+                    const location = `${city}, ${state}`
+
+                    console.log(employee);
+
+                    return (
+                        <tr key={employee.login.uuid}>
+                            <td>
+                                <img src={employee.picture.medium} alt={name} />
+                            </td>
+                            <td className="align-middle">{name}</td>
+                            <td className="align-middle">
+                                <a href={`mailto:${employee.email}`}>{employee.email}</a>
+                            </td>
+                            <td className="align-middle">
+                                <span>{location}</span>
+                            </td>
+                            <td className="align-middle">
+                                <span>{employee.phone}</span>
+                            </td>
+                            <td className="align-middle">
+                                <span>{employee.dob.age}</span>
+                            </td>
+
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 };
 
 export default EmployeeTable;
